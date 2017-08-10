d2984b9f7fb4dc138e561c2eb77d56a0afe09bb8
yanjinhui <2908116652@qq.com>
2017-08-10 13:55:15 +0800

    jz-pc

--- bin/run.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2017/1/2.
- */
-var current_path = process.cwd();
-
-console.log(current_path + '/bin/www' );
-
-require('runkoa')(current_path + '/bin/www.js' )
--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kid on 2017/1/6.
- */
-export default {
-	fetchState: state=> state.fetchState,
-	resultCode: state=> state.resultCode,
-	resultMsg: state=> state.resultMsg,
-	resultData: state=> state.resultData
-};
